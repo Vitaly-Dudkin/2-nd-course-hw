6ce46ebd1c4b4b4320e8f85d24f84fee248d0787
Виталий <dudkin231@mail.ru>
2025-05-14 16:00:09 +0300

    little changes

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// // task - 1
+// task - 1
 let a = 10 
 alert(a)
 a = 20 
 alert(a)
 
-// // task - 2
+// task - 2
 let firstIphone = 2007
 alert(`Год выпуска первого iPhone - ${firstIphone}`)
 
-// // task - 3 
+// task - 3 
 let name = 'Brendan Eich'
 alert(`Имя создателя языка JavaScript - ${name}`)
 
-// // task - 4 
+// task - 4 
 let y = 10
 let x = 2
 alert(`${y} + ${x} = ${y + x}`)
@@ -20,7 +20,7 @@ alert(`${y} - ${x} = ${y - x}`)
 alert(`${y} * ${x} = ${y * x}`)
 alert(`${y} / ${x} = ${y / x}`)
 
-// // task - 5 
+// task - 5 
 result = 2 ** 5
 alert(result, 'task - 5')
 
