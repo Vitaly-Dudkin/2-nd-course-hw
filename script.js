3c4aa38ab258a4e061f612f4abcf138325019c2f
Виталий <dudkin231@mail.ru>
2025-05-14 09:48:03 +0300

    chanhed name var

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 // // task - 1
-// let a = 10 
-// alert(a)
-// a = 20 
-// alert(a)
+let a = 10 
+alert(a)
+a = 20 
+alert(a)
 
 // // task - 2
-// let firstIphone = 2007
-// alert(`Год выпуска первого iPhone - ${firstIphone}`)
+let firstIphone = 2007
+alert(`Год выпуска первого iPhone - ${firstIphone}`)
 
 // // task - 3 
-// let name = 'Brendan Eich'
-// alert(`Имя создателя языка JavaScript - ${name}`)
+let name = 'Brendan Eich'
+alert(`Имя создателя языка JavaScript - ${name}`)
 
 // // task - 4 
-// let y = 10
-// let x = 2
-// alert(`${y} + ${x} = ${y + x}`)
-// alert(`${y} - ${x} = ${y - x}`)
-// alert(`${y} * ${x} = ${y * x}`)
-// alert(`${y} / ${x} = ${y / x}`)
+let y = 10
+let x = 2
+alert(`${y} + ${x} = ${y + x}`)
+alert(`${y} - ${x} = ${y - x}`)
+alert(`${y} * ${x} = ${y * x}`)
+alert(`${y} / ${x} = ${y / x}`)
 
 // // task - 5 
-// result = 2 ** 5
-// alert(result, 'task - 5')
+result = 2 ** 5
+alert(result, 'task - 5')
 
 // task - 6 
-// let a = 9 
-// let b = 2
-// alert(`${a} % ${b} = ${a % b}`)
+let q = 9 
+let w = 2
+alert(`${q} % ${w} = ${q % w}`)
 
 // task - 7 
-// let num = 1;
-// num += 5;
-// num -= 3;
-// num *= 7;
-// num /= 3;
-// num++;
-// num--;
-// alert(num);
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
 
 // task - 8
-// let age = Number(prompt('Сколько вам лет?'))
-// alert(age)
+let age = Number(prompt('Сколько вам лет?'))
+alert(age)
 
 // tasl - 9
 const user = {
